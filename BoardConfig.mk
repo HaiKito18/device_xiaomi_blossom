e952b1d730add8f50ccf4aa15d36cf4456990457
HaiKito18 <haininhquang3@gmail.com>
2025-10-27 21:01:56 +0700

    Blossom: monet: move TARGET_INIT_VENDOR_LIB to soong_config
    * vendor/lineage/config/BoardConfigSoong.mk:90: warning: TARGET_INIT_VENDOR_LIB is deprecated, please migrate to soong_config_set,libinit,vendor_init_lib

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,7 @@ TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 
 # Init
-$(call soong_config_set,libinit,vendor_init_lib,//$(DEVICE_PATH):init_xiaomi_monet)
+$(call soong_config_set,libinit,vendor_init_lib,libinit_blossom)
 TARGET_RECOVERY_DEVICE_MODULES := libinit_blossom
 
 # Jemalloc
